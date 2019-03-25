d466232c3edaaf1da1c81cf9cfccb2edac30fcd9
Connor Homan <connorhoman14@gmail.com>
2019-03-25 12:32:31 -0700

    Added array functionality to StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -21,21 +21,28 @@ var stringifyJSON = function(obj) {
   	return obj;
 
   } else if (obj.constructor === Number) {
-  	return number.toString();
+  	return obj.toString();
 
 
 
   } else if (obj.constructor === Array) {
 
+  	var stringifyArray = function(obj) {
+  		var first = obj[0];
 
+  		if (obj.length === 1) {
+  			return obj;
+  		}
+  		return stringifyJSON(first) + ', ' + stringifyArray(obj.slice(1)); 
+  	}
 
+  	return ('[' + stringifyArray(obj) + ']');
 
   } else if (obj.constructor === Object) {
 
+  	return '{' + function stringify(obj) {
 
-
-
-
+  	} + '}';
   }
 
 
