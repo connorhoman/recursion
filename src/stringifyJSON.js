76eeda7efd1ebdfacfc8c53fe5b84da2f911759e
Connor Homan <connorhoman14@gmail.com>
2019-03-25 15:40:00 -0700

    Finished stringifyJSON implementation

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,46 +4,73 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if (obj === undefined) {
-  	return 'undefined';
+	if (obj === undefined) {
+		return undefined;
+	}
 
-  } else if (obj === null) {
-  	return 'null';
+	if (obj === null) {
+  		return 'null';
+	}
 
-  } else if (obj.constructor === Boolean) {
-  	if (obj = true) {
-  		return 'true';
-  	} else {
+	if (typeof(obj) === 'boolean') {
+  		if (obj === true) {
+  			return 'true';
+  		} else if (obj === false) {
   		return 'false';
-  	}
-
-  } else if (obj.constructor === String) {
-  	return obj;
-
-  } else if (obj.constructor === Number) {
-  	return obj.toString();
-
-  } else if (obj.constructor === Array) {
-  	var stringifyArray = function(obj) {
-  		var first = obj[0];
-
-  		if (obj.length === 1) {
-  			return obj;
   		}
-  		return stringifyJSON(first) + ', ' + stringifyArray(obj.slice(1)); 
   	}
-  	return ('[' + stringifyArray(obj) + ']');
 
 
+  	if (typeof(obj) === 'string') {
+  		return '"' + obj + '"';
+	}
 
 
-  } else if (obj.constructor === Object) {
-  	var stringifyObject = function(obj) {
+	if (typeof(obj) === 'number') {
+  		return obj.toString();
+	}
 
-  		
+
+  	if (obj.constructor === Array) {
+  		if (obj.length === 0) {
+  			return "[]";
+  		}
+  		var stringifyArray = function(obj) {
+  			var first = obj[0];
+
+  			if (obj.length === 1) {
+  				return stringifyJSON(first);
+  			}
+  			return stringifyJSON(first) + ',' + stringifyArray(obj.slice(1)); 
+  		}
+  		return ('[' + stringifyArray(obj) + ']');
   	}
-  	return '{' + stringifyObject(obj) + '}';
-  }
+
+
+  	if (obj.constructor === Object) {
+  		for (key in obj) {
+  			if (obj[key] === undefined || typeof(obj[key]) === 'function') {
+  				delete obj[key];
+  			}
+  		}
+  		var keys = Object.keys(obj);
+  		if (keys.length === 0) {
+  			return "{}";
+  		}
+
+	  	var stringifyObject = function(obj, keys) {
+	  		if (keys.length === 1) {
+	  			return stringifyJSON(keys[0]) + ':' + stringifyJSON(obj[keys[0]]);
+	  		}
+	  		if (obj[keys[0]] === undefined) {
+	  			return stringifyObject(obj, keys.slice(1));
+	  		}
+	  		return stringifyJSON(keys[0]) + ':' + stringifyJSON(obj[keys[0]]) + ',' + stringifyObject(obj, keys.slice(1));
+  		}
+  		return '{' + stringifyObject(obj, Object.keys(obj)) + '}';
+  } 
+
+
 
 
 };
